8ccaf14711ceb76071d997a985766cc57a3a9a72
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-01-10 14:58:51 +0800

    create a midlware for manual error

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../model/user.js";
 import bcryptjs from 'bcryptjs';
+// import { errorHandler } from "../utilities/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
    const { username , email, password } = req.body;
    const hidePassword = bcryptjs.hashSync(password, 10);
    const newUser = new User ({ username, email, password:hidePassword});
@@ -10,7 +11,7 @@ export const signup = async (req, res) => {
     res.status(201).json('User Created Succesfully');
    }
    catch(err){
-    res.status(500).json(err.message)
+    next(err)
    }
  
   
--- api/index.js
+++ api/index.js
@@ -22,4 +22,13 @@ app.listen(3000, ()=>{
 );
 
 app.use("/api/user", UserRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message,
+    });
+});
