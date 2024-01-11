b8ae515a414c1d1a3a64653365a432d1b4e74fed
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-01-11 16:15:08 +0800

    Create SignIn APi

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../model/user.js";
 import bcryptjs from 'bcryptjs';
-// import { errorHandler } from "../utilities/error.js";
+import { errorHandler } from "../utilities/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
    const { username , email, password } = req.body;
@@ -13,6 +14,24 @@ export const signup = async (req, res, next) => {
    catch(err){
     next(err)
    }
- 
-  
+};
+
+export const SignIn = async (req, res, next) => {
+   const {email, password } = req.body;
+   // const hidePassword = bcryptjs.hashSync(password, 10);
+   // const newUser = new User ({ username, email, password:hidePassword});
+   try{
+      const validUser = await User.findOne({email});
+         if(!validUser)
+         return next(errorHandler(404, 'User Not Found'));
+      const validPass = bcryptjs.compareSync(password, validUser.password);
+         if(!validPass)
+         return next(errorHandler(401, 'Wrong Credentials!'));
+      const webToken = jwt.sign({ id: validUser._id },process.env.JWT_Secret);
+      const { password: pass, ...rest} = validUser._doc;
+      res.cookie('Access_Token', webToken, { httponly: true }).status(200).json(rest);
+   }
+   catch(err){
+    next(err)
+   }
 };
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controller/auth.controller.js";
+import { SignIn, signup } from "../controller/auth.controller.js";
 
 const Router = express.Router();
 
 Router.post('/signup', signup);
+Router.post('/sign-in', SignIn);
 
 export default Router
